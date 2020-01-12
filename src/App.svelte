62e5f04137f676739549f2ecd190cbb9e8d22def
Burak Kaplan <burak@kaplan.dev>
2020-01-12 17:04:15 +0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<style>
+    .app {
+        width: 100vw;
+        height: 100vh;
+        display: grid;
+        place-items: center;
+        background: black;
+    }
+
+    h1 {
+        color: white;
+    }
+</style>
+
+<div class="app">
+    <h1>12</h1>
+</div>
